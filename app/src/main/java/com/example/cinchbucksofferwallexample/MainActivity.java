a556b6245ab6b520e3e0c32930010d378957f2bd
skondgekar <skondgekar@yahoo.co.in>
2020-07-14 01:01:02 +0530

    Initial Commit. Simple example app with a button

---
+++ app/src/main/java/com/example/cinchbucksofferwallexample/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.cinchbucksofferwallexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("SYK","Button is clicked");
+            }
+        });
+    }
+}
