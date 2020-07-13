46696808e8818bd4e4414a959e3b5d96128dc8d4
skondgekar <skondgekar@yahoo.co.in>
2020-07-14 02:19:28 +0530

    All Added

--- app/src/main/java/com/example/cinchbucksofferwallexample/MainActivity.java
+++ app/src/main/java/com/example/cinchbucksofferwallexample/MainActivity.java
@@ -7,19 +7,79 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.cbofferwallsdk.CbOfferwall;
+import com.example.cbofferwallsdk.CbOfferwallRewardListener;
+import com.example.cbofferwallsdk.CbOfferwallSurveyAvailableListener;
+import com.example.cbofferwallsdk.CbOfferwallSurveyListener;
+
+public class MainActivity extends AppCompatActivity implements CbOfferwallRewardListener, CbOfferwallSurveyListener, CbOfferwallSurveyAvailableListener  {
+
+    private final String TAG = "CbOfferwall";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //initialize CbOfferwall
+        CbOfferwall.initWithApiKeyAndUserIdAndActivityContext("40cdb7704cacbaeb4c4e491f4ece", "ANDROID_TEST_ID", this);
+
+        //customize navigation header
+        CbOfferwall.getInstance().setNavigationBarText("Demo App");
+        CbOfferwall.getInstance().setNavigationBarColor("#211548");
+        CbOfferwall.getInstance().setNavigationBarTextColor("#FFFFFF");
+
+        //set reward and survey status listeners
+        CbOfferwall.getInstance().setCbOfferwallRewardListener(this);
+        CbOfferwall.getInstance().setCbOfferwallSurveyListener(this);
+        CbOfferwall.getInstance().setCbOfferwallSurveyAvailableListener(this);
+
         Button btn = (Button) findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("SYK","Button is clicked");
+                if (CbOfferwall.getInstance().isSurveyAvailable()) {
+                    // for second placement to earn Gems as well as Tokens
+//                    CbOfferwall.getInstance().showRewardCenter("66cb0225-3af3-4d63-8920-7a7a9e43abb2");
+                    CbOfferwall.getInstance().showRewardCenter();
+                }
+
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        CbOfferwall.getInstance().onResume(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        CbOfferwall.getInstance().onPause();
+    }
+
+    @Override
+    public void onReward(int i) {
+        Log.d(TAG, "onReward: " + i);
+
+    }
+
+    @Override
+    public void onRewardCenterClosed() {
+        Log.d(TAG, "onRewardCenterClosed");
+    }
+
+    @Override
+    public void onRewardCenterOpened() {
+        Log.d(TAG, "onRewardCenterOpened");
+    }
+
+    @Override
+    public void cbofferwallSurveyAvailable(boolean surveyAvailable) {
+        Log.d(TAG, "CbOfferwall Survey Available: " + surveyAvailable);
+
     }
 }
